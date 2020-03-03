1476961edd109c99b6bccb2bc0f4b1b0a91783a3
Beni Cakitaki <b.cakitaki@latrobe.edu.au>
2020-03-03 12:22:28 +1100

    A commit from BC local

--- R/Parent_attain_function.R
+++ R/Parent_attain_function.R
@@ -1,9 +1,9 @@
 # Parent_attain()
 
-# This function reads in a .csv of La Trobe enrolment parent attainment data, 
+# This function reads in a .csv of La Trobe enrolment parent attainment data,
 # and converts it into a standardised format
 
-# NOTE: You will need to at least include the following variables in the LTU BO extract: 
+# NOTE: You will need to at least include the following variables in the LTU BO extract:
 
         # - `Student ID`
         # - `Parent Highest Educ Attainment Code`
@@ -11,38 +11,38 @@
 
 # Create a function that converts parent highest ed into standard highest ed measure
 Parent_attain <- function(path) {
-  
-  x <- readr::read_csv(path) %>% 
-    dplyr::rename_all(make.names) %>% 
-    dplyr::mutate_all(as.character) %>% 
+
+  x <- readr::read_csv(path) %>%
+    janitor::clean_names() %>%
+    dplyr::mutate_all(as.character) %>%
     dplyr::distinct()
-  
-  if ("Parent.Highest.Educ.Attainment.Code" %in% colnames(x) == F) {
-    stop("Sigh...\n You are missing the variable 'Parent.Highest.Educ.Attainment.Code' from your df.\n Its really not that hard...\n Please just go back and get it.")   
+
+  if ("parent_highest.Educ.Attainment.Code" %in% colnames(x) == F) {
+    stop("Sigh...\n You are missing the variable 'Parent.Highest.Educ.Attainment.Code' from your df.\n Its really not that hard...\n Please just go back and get it.")
   } else if ("Parent.Highest.Educ.Attainment" %in% colnames(x) == F) {
     stop("Sigh...\n You are missing the variable 'Parent.Highest.Educ.Attainment' from your df.\n Its really not that hard...\n Please just go back and get it.")
   } else if ("Student.ID" %in% colnames(x) == F) {
     stop("Sigh...\n You are missing the 'Student.ID' from your df.\n Well done... Very on brand for La Trobe.\n Please just go back and get it.")
   }
   message("You have all the right variables, and they are all correctly named! Excellent work comrade!")
-  
+
   x <- x %>%
-    mutate(
-      Ed.hierarchy = case_when(
+    dplyr::mutate(
+      Ed.hierarchy = dplyr::case_when(
         Parent.Highest.Educ.Attainment.Code == "PG" ~ 1,
         Parent.Highest.Educ.Attainment.Code == "BACH" ~ 2,
         T ~ 3
       )
-    ) %>% 
-    group_by(Student.ID) %>% 
-    mutate(Parent.attainment = min(Ed.hierarchy)) %>% 
-    mutate(Parent.attainment = case_when(
+    ) %>%
+    dplyr::group_by(Student.ID) %>%
+    dplyr::mutate(Parent.attainment = min(Ed.hierarchy)) %>%
+    dplyr::mutate(Parent.attainment = case_when(
       Parent.attainment == 1 ~ "Postgraduate",
       Parent.attainment == 2 ~ "Bachelor",
       Parent.attainment == 3 ~ "No HE"
-    )) %>% 
-    ungroup() %>% 
-    distinct(Student.ID,Parent.attainment)
+    )) %>%
+    dplyr::ungroup() %>%
+    dplyr::distinct(Student.ID,Parent.attainment)
 }
 
 
